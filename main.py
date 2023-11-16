f5c7de34ac5104c0ef3c9c30da0fe7a2e1859234
Eduardo <eduardoramirez0104@gmail.com>
2023-11-15 22:47:27 -0400

    ajustes para reportar cada 5 minutos

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+User
 import RPi.GPIO as GPIO
 import Adafruit_DHT
 from gpiozero import DigitalInputDevice
 import time
 import threading
+from datetime import datetime
 
 import firebase_admin
 from firebase_admin import credentials
@@ -14,11 +16,6 @@ firebase_admin.initialize_app(cred)
 
 # Create a Firestore client
 db = firestore.client()
-
-# Reference to the Firestore collection and create a new document
-dht11_ref = db.collection('dht11_data').document()
-moisture_ref = db.collection('moisture_sensor').document()
-bomba_ref = db.collection('bomba').document()
 
 # Configuración de los pines GPIO
 pin_relay = 23
@@ -33,61 +30,59 @@ MOISTURE_SENSOR = DigitalInputDevice(moisture_pin)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin_relay, GPIO.OUT)
 
-# Variable para el control de la bomba y el lock para sincronización
-bomba_encendida = False
-lock = threading.Lock()
+# Variable para controlar la ejecución del bucle
+running = True
 
 # Función para controlar la bomba y registrar eventos
 def controlar_bomba(encendida):
-    global bomba_encendida
+    GPIO.output(pin_relay, GPIO.HIGH if encendida else GPIO.LOW)
+    estado = "Encendida" if encendida else "Apagada"
 
-    with lock:
-        GPIO.output(pin_relay, GPIO.HIGH if encendida else GPIO.LOW)
-        estado = "Encendida" if encendida else "Apagada"
+    # Crear un nuevo documento para registrar el evento de encendido o apagado
+    bomba_ref = db.collection('bomba').document()
+    bomba_ref.set({
+        'estado': estado,
+        'timestamp': firestore.SERVER_TIMESTAMP
+    })
 
-        # Registra el evento solo si hay un cambio en el estado
-        if bomba_encendida != encendida:
-            bomba_ref.set({
-                'estado': estado,
-                'timestamp': firestore.SERVER_TIMESTAMP
-            })
-            print(f"Bomba {estado}")
+    print(f"Bomba {estado}")
 
-        bomba_encendida = encendida
-
-# Función para detectar temperatura y humedad cada minuto
-def monitorear_temperatura_humedad():
-    while True:
-        # Leer la temperatura y la humedad
+# Función para leer la temperatura y actualizar Firestore
+def monitorear_temperatura():
+    while running:
         humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 
         if humidity is not None and temperature is not None:
             print("Temperatura={0:0.1f}°C Humedad={1:0.1f}%".format(temperature, humidity))
 
-            # Set the data in the document
+            # Crear un nuevo documento para cada lectura
+            dht11_ref = db.collection('dht11_data').document()
             dht11_ref.set({
                 'temperature': temperature,
                 'humidity': humidity,
                 'moisture': not MOISTURE_SENSOR.value,  # Reverse the moisture value
                 'timestamp': firestore.SERVER_TIMESTAMP
             })
+
+            time.sleep(60)  # Esperar 1 minuto
         else:
             print("No se pudo leer la temperatura")
 
-        time.sleep(60)  # Espera 60 segundos antes de la próxima lectura
-
-# Inicia el hilo para monitorear temperatura y humedad
-thread_temperatura_humedad = threading.Thread(target=monitorear_temperatura_humedad)
-thread_temperatura_humedad.start()
+# Hilo para monitorear la temperatura
+temperature_thread = threading.Thread(target=monitorear_temperatura)
 
 print("AgroTech Sistema de Monitoreo")
 
 try:
+    # Iniciar el hilo de monitoreo de temperatura
+    temperature_thread.start()
+
     while True:
         if not MOISTURE_SENSOR.value:
             print("Agua detectada")
 
-            # Registra el evento de detección de agua
+            # Crear un nuevo documento para registrar el evento de detección de agua
+            moisture_ref = db.collection('moisture_sensor').document()
             moisture_ref.set({
                 'estado': 'Se detectó agua',
                 'timestamp': firestore.SERVER_TIMESTAMP
@@ -95,11 +90,12 @@ try:
 
             bomba_encendida = False  # Apagar la bomba cuando se detecta agua
             controlar_bomba(bomba_encendida)
-
+            time.sleep(30)  # Esperar 30 segundos antes de verificar nuevamente
         else:
             print("No se detecta agua")
 
-            # Registra el evento de no detección de agua
+            # Crear un nuevo documento para registrar el evento de no detección de agua
+            moisture_ref = db.collection('moisture_sensor').document()
             moisture_ref.set({
                 'estado': 'No se detectó agua',
                 'timestamp': firestore.SERVER_TIMESTAMP
@@ -111,4 +107,6 @@ try:
         time.sleep(2)
 
 except KeyboardInterrupt:
+    running = False  # Detener el hilo de monitoreo de temperatura
     GPIO.cleanup()
+    temperature_thread.join()  # Esperar a que el hilo termine
